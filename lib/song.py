a355247743db54918e9237bc837db31b916edd01
cynthiawuor <cynthiawuor254@gmail.com>
2023-10-06 13:12:52 +0300

    song

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,21 @@
 from config import CONN, CURSOR
+from song import Song
+
+import ipdb; ipdb.set_trace()
 
 class Song:
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT
+            EXISTS songs (id INTEGER PRIMARY KEY, name TEXT, album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        
     pass
